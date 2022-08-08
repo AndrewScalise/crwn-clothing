3155e5de02302ac172813d04f00c25a671632baa
Mugen <mugennewjabes@gmail.com>
2022-08-08 18:24:43 -0500

    add product cards

--- crwn-clothing-app/src/contexts/products.context.jsx
+++ crwn-clothing-app/src/contexts/products.context.jsx
@@ -1,17 +1,17 @@
 import { createContext, useState } from "react";
-import Products from "../shop-data.json";
+
+import PRODUCTS from "../shop-data.json";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(Products);
+  const [products, setProducts] = useState(PRODUCTS);
   const value = { products };
-
   return (
-    <ProductsProvider.Provider value={value}>
+    <ProductsContext.Provider value={value}>
       {children}
-    </ProductsProvider.Provider>
+    </ProductsContext.Provider>
   );
 };
